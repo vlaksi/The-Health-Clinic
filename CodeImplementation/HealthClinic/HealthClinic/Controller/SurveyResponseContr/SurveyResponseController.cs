84913ba94ce141ddc9f78cda0e39520d3658f1da
vlaksi <daksrapsody@gmail.com>
2020-06-20 00:06:32 +0200

    Zavrseno paljenje masine, kontroler ubacen, potrebno jos resiti neke stvari ali to ce Pufke resiti

---
+++ CodeImplementation/HealthClinic/HealthClinic/Controller/SurveyResponseContr/SurveyResponseController.cs
@@ -0,0 +1,34 @@
+// File:    SurveyResponseController.cs
+// Author:  Vaxi
+// Created: Tuesday, May 19, 2020 11:39:18 PM
+// Purpose: Definition of Class SurveyResponseController
+
+using Model.Survey;
+using Service.SurveyResponseServ;
+using System;
+using System.Collections.Generic;
+
+namespace Controller.SurveyResponseContr
+{
+   public class SurveyResponseController
+   {
+      public SurveyResponseService surveyResponseService;
+
+      public List<SurveyResponse> GetAllServeyResponses()
+      {
+         throw new NotImplementedException();
+      }
+      
+      public void AddServeyResponses()
+      {
+         throw new NotImplementedException();
+      }
+      
+      public void DelteServeyResponse()
+      {
+         throw new NotImplementedException();
+      }
+      
+   
+   }
+}
