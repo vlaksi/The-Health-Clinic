1c2947f165b35f26256bf4d14b7b0d035b3c0000
vlaksi <daksrapsody@gmail.com>
2020-06-21 04:46:17 +0200

    Ubacen model zaposlenog + u Person ubaceni rodjendan, adresa stanovanja, biografija, kontakt telefon

---
+++ HealthClinic/HealthClinic/Model/Users/Employee.cs
@@ -0,0 +1,30 @@
+﻿using Model.BusinessHours;
+using Model.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthClinic.Model.Users
+{
+    public class Employee : RegisteredUser
+    {
+		private string _jobPosition;
+		private BusinessHoursModel _businessHours;
+
+		public BusinessHoursModel BusinessHours
+		{
+			get { return _businessHours; }
+			set { _businessHours = value; OnPropertyChanged("BusinessHours"); }
+		}
+
+
+		public string JobPosition
+		{
+			get { return _jobPosition; }
+			set { _jobPosition = value; OnPropertyChanged("JobPosition"); }
+		}
+
+	}
+}
