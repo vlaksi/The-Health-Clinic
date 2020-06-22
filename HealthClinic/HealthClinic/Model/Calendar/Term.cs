7f10a1c2572899c192f9b5b62947c722fec6a335
vlaksi <daksrapsody@gmail.com>
2020-06-22 23:30:33 +0200

    Pokupljene promene od tima

--- HealthClinic/HealthClinic/Model/Calendar/Term.cs
+++ HealthClinic/HealthClinic/Model/Calendar/Term.cs
@@ -4,87 +4,99 @@
 // Purpose: Definition of Class Term
 
 using HealthClinic.Utilities;
-using System;
+//using Syncfusion.UI.Xaml.Schedule;
+using System.ComponentModel;
 
 namespace Model.Calendar
 {
-   public class Term : ObservableObject
-    {
-      private System.DateTime startTime;
-      private System.DateTime endTime;
-      private int id;
 
-      public System.DateTime StartTime
-      {
-         get
-         {
-            return startTime;
-         }
-         set
-         {
-            this.startTime = value;
-            OnPropertyChanged("StartTime");
-         }
-      }
-      
-      public System.DateTime EndTime
-      {
-         get
-         {
-            return endTime;
-         }
-         set
-         {
-            this.endTime = value;
-            OnPropertyChanged("EndTime");
-         }
-      }
-      
-      public int Id
-      {
-         get
-         {
-                return id;
-         }
-         set
-         {
-               this.id = value;
-               OnPropertyChanged("Id");
-         }
-      }
-      
-      public Report report;
-      public Model.Users.SpecialtyType specialtyType;
-      public Model.MedicalRecord.MedicalRecord medicalRecord;
-      
-      /// <summary>
-      /// Property for Model.MedicalRecord.MedicalRecord
-      /// </summary>
-      /// <pdGenerated>Default opposite class property</pdGenerated>
-      public Model.MedicalRecord.MedicalRecord MedicalRecord
-      {
-         get
-         {
-            return medicalRecord;
-         }
-         set
-         {
-            if (this.medicalRecord == null || !this.medicalRecord.Equals(value))
+    //public class Term : ScheduleAppointment, INotifyPropertyChanged
+    public class Term : ObservableObject
+    {
+        /*
+         * Naslijedjeni od strane ScheduleAppointment 
+         * private System.DateTime startTime;
+        private System.DateTime endTime;
+
+        public System.DateTime StartTime
+        {
+           get
+           {
+              return startTime;
+           }
+           set
+           {
+              this.startTime = value;
+           }
+        }
+        public System.DateTime EndTime
+        {
+           get
+           {
+              return endTime;
+           }
+           set
+           {
+              this.endTime = value;
+           }
+        }*/
+
+        private int id;
+        public int Id
+        {
+            get
             {
-               if (this.medicalRecord != null)
-               {
-                  Model.MedicalRecord.MedicalRecord oldMedicalRecord = this.medicalRecord;
-                  this.medicalRecord = null;
-                  oldMedicalRecord.RemoveTerm(this);
-               }
-               if (value != null)
-               {
-                  this.medicalRecord = value;
-                  this.medicalRecord.AddTerm(this);
-               }
+                return id;
             }
-         }
-      }
-   
-   }
+            set
+            {
+                this.id = value;
+            }
+        }
+
+        private Report report;
+
+        public Report Report
+        {
+            get { return report; }
+            set { report = value; }
+        }
+
+        private Model.Users.SpecialtyType specialtyType;
+
+        public Model.Users.SpecialtyType SpecialtyType
+        {
+            get { return specialtyType; }
+            set { specialtyType = value; }
+        }
+
+
+        private Model.MedicalRecord.MedicalRecord medicalRecord;
+
+        public Model.MedicalRecord.MedicalRecord MedicalRecord
+        {
+            get
+            {
+                return medicalRecord;
+            }
+            set
+            {
+                if (this.medicalRecord == null || !this.medicalRecord.Equals(value))
+                {
+                    if (this.medicalRecord != null)
+                    {
+                        Model.MedicalRecord.MedicalRecord oldMedicalRecord = this.medicalRecord;
+                        this.medicalRecord = null;
+                        oldMedicalRecord.RemoveTerm(this.Id);
+                    }
+                    if (value != null)
+                    {
+                        this.medicalRecord = value;
+                        this.medicalRecord.AddTerm(this.Id);
+                    }
+                }
+            }
+        }
+
+    }
 }
